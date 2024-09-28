9ec6aa19437d7a88e599d35589c8d9133558d805
Hamza Başaran <1basaranhamza@gmail.com>
2024-09-28 22:03:45 +0300

    Lesson3

--- Workspace/Lesson2/app.js
+++ Workspace/Lesson2/app.js
@@ -1,1 +1,23 @@
-console.log("Lesson1");
+// document .write("Hello World !<br>");
+// document.write("Enes Bayram");
+// document.write(true);
+// document.write(5.5);
+
+// console.log("HelloWorld");
+// console.clear();//console temizler
+
+// let a=5;
+// let b=10;
+// console.log("iki sayı top",a+b)
+
+// console.log(window.alert);
+// console.log(document.location.host);
+
+// alert("Merhaba");
+// debugger;
+// let a=5;
+// let b=10;
+// alert.log("iki sayı top",a+b)
+
+//  console.log(window.location.port);
+// !...
--- Workspace/Lesson3/app.js
+++ Workspace/Lesson3/app.js
@@ -1,1 +1,37 @@
 /*
+    Scope(Kapsam)
+    Global Scope
+    Function Scope
+    Block Scope
+    var-let - const
+*/
+
+// var değişkenIsmi = 10 ;
+// console.log(değişkenIsmi);
+
+// var a = 5 ;//global scope  
+//  while(a>4)
+//     {
+//         console.log(a);        
+//         break;
+//     }
+//     if(true)
+//     {
+//        function method1()
+//        {
+//         console.log(a);
+//        } 
+//     }
+//     method1 ();
+
+//! Function Scope
+// function method1(){
+//     var sayi =10;
+//     console.log (sayi);
+// }
+//! Block scope
+function method1(){
+    if(true){
+        var a=5; //block 
+    }
+}
