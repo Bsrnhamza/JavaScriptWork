247f83839e5e17d9e9fd03988c3c3e00d093808b
Hamza Başaran <1basaranhamza@gmail.com>
2024-09-29 12:07:06 +0300

    Lesson4

---
+++ Workspace/Lesson4/app.js
@@ -0,0 +1,31 @@
+//Operotorler
+var sonuc;
+const x=16;
+const y=11;
+let z = 2 ;
+let t = 2;
+//!Aritmatik
+sonuc = x+y;
+sonuc = x-y;
+sonuc = x*y;
+sonuc = x/y;
+sonuc = x%y;
+sonuc = z++;//hafızada
+sonuc = ++z;//ekranda
+//!Atama
+sonuc = x ;
+sonuc += x ;
+sonuc -= x ;
+sonuc *= x ;
+sonuc /= x ;
+sonuc %= x ;
+//!Karşılaştırma
+z=9;
+t ="9";
+sonuc= x == y ;
+sonuc= x === y ;//typeof da kontrol edilir
+sonuc = x != y ; //true
+sonuc =z!= t ; //false
+sonuc =z < t ;
+sonuc =z > t ;
+//!Mantıksal
