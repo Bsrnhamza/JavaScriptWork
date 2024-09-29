226026aabed1f5b31e74b5a07e8ed84364f46e2b
Hamza Başaran <1basaranhamza@gmail.com>
2024-09-29 12:58:08 +0300

    Lesson6

---
+++ Workspace/Lesson5/app.js
@@ -0,0 +1,13 @@
+ // Date Object
+ var x = new Date();
+ console.log(x);
+ //!Get
+console.log(x.getFullYear());
+console.log(x.getMonth());
+console.log(x.getDate());
+console.log(x.getHours());
+console.log(x.getDay());
+ //!Set
+ x.setFullYear(1996);
+ x.setMonth(10);
+ x.setDate(25);
---
+++ Workspace/Lesson6/app.js
@@ -0,0 +1,21 @@
+const ad ="Hamza";
+const soyad="Basaran";
+
+var x = ad +" "+soyad;
+
+x = ad.concat(" ",soyad);
+
+x = ad.length;
+x=soyad.length;
+
+x = ad.toUpperCase();
+x = soyad.toLowerCase();
+
+x = x.indexOf("B");
+
+x = x.slice(2);
+
+x = x.replace("Hamza" ,"Ahmet");
+
+
+console.log(x);
